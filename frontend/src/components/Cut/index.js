a0dd48f48abfcffada216091f07aa2de273ae992
kapitan_kb <19vano97@gmail.com>
2020-05-15 10:18:25 +0500

    add cut edit menu

--- frontend/src/components/Cut/index.js
+++ frontend/src/components/Cut/index.js
@@ -8,7 +8,24 @@ class Cut extends Component {
         return (
             <div className='cut edit-menu'>
                 <nav id='cut-menu' className='menu-right'>
-                    <div className='underconstruction'><img src={"/images/underconstruction.png"} alt="horizontally"/></div>
+                    <div className='rotate-tool'>
+                        <label className='title'>Обрезать</label>
+                        <label>Произвольно</label>
+                        <div className='buttons f'>
+                            <div className='buttonu y'><div className='ok'>957</div></div>
+                            <img src={"icons/x.svg"} alt="x"/>
+                            <div className='buttonu y'><div className='ok'>756</div></div>
+                        </div>
+                        <div className='buttonu save'><div className='oks'>Сохранять пропорции</div></div>
+                    </div>
+                    <div className='rotate-tool'>
+                        <label>Фигурно</label>
+                        <div className='buttons'>
+                            <div className='buttonu'><img src={"icons/circle.svg"} alt="left"/></div>
+                            <div className='buttonu'><img src={"icons/ellipse.svg"} alt="right"/></div>
+                            <div className='buttonu'><img src={"icons/heart.svg"} alt="right"/></div>
+                        </div>
+                    </div>
                 </nav>
                 <EditField/>
             </div>
